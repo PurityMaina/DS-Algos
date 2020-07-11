cd0458dfc92d6454f3882b8cfb778a2c796c4f92
Purity Maina <puritymaina001@gmail.com>
2020-07-11 23:56:11 +0300

    summary or camparison of the ds

---
+++ data_structures(built_in)/summary.py
@@ -0,0 +1,16 @@
+#Lists are enclosed in brackets:
+l = [1, 2, "a"]
+print(l)
+#Tuples are enclosed in parentheses:
+t = (1, 2, "a")
+print(t)
+#Tuples are faster and consume less memory.
+
+#Dictionaries are built with curly brackets:
+d = {"a":1, "b":2}
+print(d)
+
+#Sets are made using the set() builtin function
+s = set([1, 2, 3, 4])
+print(s)
+map
