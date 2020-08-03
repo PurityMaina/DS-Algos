2b39e6bbecca3619254181ee8cbd07e7594fc378
Purity Maina <puritymaina001@gmail.com>
2020-08-03 11:12:29 +0300

    working on type conversions in python

---
+++ type_conversions.py
@@ -0,0 +1,27 @@
+#string to int
+s = "10001"
+conversion = int(s)
+print("After converting to integer: ", conversion)
+#string to float
+f = float(s)
+print("After converting to float: ", f)
+#string to tuple.
+s = "geeks for geeks"
+t = tuple(s)
+print("After converting to tuple : ", t)
+#to set -- a collection which is unordered and unindexed
+conv = set(s)
+print("After converting to set : ", conv)
+#to list
+li = list(s)
+print("After converting to list : ", li)
+#tuple to dictionary
+#nestsed-lists
+#dictionary type conversions
+test_dict = {'1' : ['4', '5'], '4' : ['6', '7'], '10' : ['8']}
+print("The original dictionary : " + str(test_dict))
+
+
+
+
+
